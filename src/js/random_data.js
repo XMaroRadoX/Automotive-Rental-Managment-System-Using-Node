11b9c9182529f62137d0ced5a5b4194fca95524d
Marwan Khaled Mohamed Radwan <marwanradwan88@gmail.com>
2022-12-10 10:57:52 +0200

    cars_specs generation finished but need fixing

--- src/js/random_data.js
+++ src/js/random_data.js
@@ -33,11 +33,11 @@ const cont = async () => {
 const user = async () => {
   try {
     const res = await fetch(`https://randomuser.me/api/?results=10`);
-    const data = await res.json();
+    const data = (await res.json()).results;
     const users = [];
-    data.forEach((user) => {
+    data.forEach((user, i) => {
       users.push({
-        id: makeid(math.random()*10),
+        id: Date.now() + i * 1000,
         fname: user.name.first,
         lname: user.name.last,
         email: user.email,
@@ -53,32 +53,119 @@ const user = async () => {
   }
 };
 // ####################### CAR DATA GENERATION ####################
+const transmissions = ["manual", "automatic", "cvt"];
+const colors = [
+  "white",
+  "black",
+  "gray",
+  "silver",
+  "blue",
+  "red",
+  "brown",
+  "green",
+  "orange",
+  "beige",
+  "purple",
+  "gold",
+  "yellow",
+];
+const types = [
+  "micro",
+  "sedan",
+  "hatchback",
+  "coupe",
+  "cabriolet",
+  "limousine",
+  "sport",
+  "suv",
+  "crossover",
+  "pickup",
+  "van",
+  "minivan",
+  "minibus",
+];
+const brands = [
+  {
+    brand: "audi",
+    families: [
+      "a1",
+      "a3",
+      "a4",
+      "a5",
+      "a6",
+      "a7",
+      "a8",
+      "q2",
+      "q3",
+      "q4",
+      "q5",
+      "q7",
+      "q8",
+      "r8",
+      "tt",
+    ],
+  },
+  { brand: "aston martin", families: ["db11", "dbx"] },
+  { brand: "bentley", families: ["continental-gt", "bentayga"] },
+  {
+    brand: "bmw",
+    families: [
+      "i3",
+      "i4",
+      "i7",
+      "i8",
+      "ix",
+      "x1",
+      "x2",
+      "x3",
+      "x4",
+      "x5",
+      "x6",
+      "x7",
+      "z4",
+    ],
+  },
+  { brand: "bugatti", families: ["chiron"] },
+  { brand: "cadillac", families: ["ct4", "ct5", "ct6", "cts"] },
+  { brand: "chevrolet", families: ["aveo", "camaro", "colorado", "cruze"] },
+  { brand: "dodge", families: ["challenger", "charger"] },
+  { brand: "fiat", families: ["500", "124-spider", "doblo"] },
+  { brand: "ford", families: ["gt", "ka", "ranger"] },
+  { brand: "hyundai", families: ["accent", "bayon"] },
+  { brand: "jeep", families: ["wrangler", "cherokee"] },
+  { brand: "kia", families: ["carens", "sportage"] },
+  {
+    brand: "mercedes",
+    families: ["cls", "sprinter", "e-class", "a-class", "b-class", "s-class"],
+  },
+  { brand: "mg", families: ["gs", "hs", "zs"] },
+  { brand: "mini", families: ["mini", "coupe"] },
+  { brand: "mitsubishi", families: ["lancer-evolution"] },
+  { brand: "nissan", families: ["gt-r", "370z"] },
+  { brand: "porsche", families: ["911", "panamera"] },
+  { brand: "subaru", families: ["ascent", "solterra"] },
+  { brand: "tesla", families: ["model-x", "model-s"] },
+  { brand: "toyota", families: ["prius"] },
+];
 const car = async () => {
-  try {
-    const res = await fetch(`http://api.carsxe.com/`);
-    const data = await res.json();
-    const cars = [];
-
-    data.forEach((car) => {
-      cars.push({
-        id: car.id,
-        brand: car.brand,
-        model: car.model,
-        type: types[Math.floor(Math.random() * types.length)],
-        color: colors[Math.floor(Math.random() * colors.length)],
-        year: Math.floor(Math.random() * 30) + 1990,
-        price: Math.floor(Math.random() * 1000) + 200,
-        seats: Math.floor(Math.random() * 6) + 2,
-        doors: Math.floor(Math.random() * 2) + 2,
-        img: car.picture.large,
-        available: Math.random() >= 0.5,
-        country: countries[Math.floor(Math.random() * countries.length)],
-        user: users[Math.floor(Math.random() * users.length)],
-      });
+  const cars_specs = [];
+  for (let index = 0; index < 1000; index++) {
+    cars_specs.push({
+      id: Date.now() + i * 781,
+      brand: brands[Math.floor(Math.random() * brands.length)].brand,
+      model:
+        brands[Math.floor(Math.random() * brands.length)].families[
+          Math.floor(Math.random() * families.length)
+        ],
+      type: types[Math.floor(Math.random() * types.length)],
+      color: colors[Math.floor(Math.random() * colors.length)],
+      year: Math.floor(Math.random() * 30) + 1990,
+      seating: Math.floor(Math.random() * 14) + 2,
+      photo: `https:cdn.imagin.studio/getImage?customer=egalexandria-university-faculty-of-engineering&make=${brand}&modelfamily=${model}&paintId=imagin-${color}`,
+      transmission:
+        transmissions[Math.floor(Math.random() * transmissions.length)],
+      rate: Math.floor(Math.random() * 1000) + 200,
     });
-    cars.sort((a, b) => a.id.localeCompare(b.id));
-  } catch (err) {
-    console.log(err);
   }
 };
 
@@ -87,11 +174,11 @@ const rental = async () => {
   try {
     const res = await fetch(`https://randomuser.me/api/`);
     const data = await res.json();
-    const rentals = [];
+    const reservations = [];
 
     data.forEach((rental) => {
-      rentals.push({
-        id: rental.id,
+      reservations.push({
+        reservation_id: Date.now() + i * 378,
         user: users[Math.floor(Math.random() * users.length)],
         car: cars[Math.floor(Math.random() * cars.length)],
         start: rental.start,
@@ -101,7 +188,7 @@ const rental = async () => {
         available: Math.random() >= 0.5,
       });
     });
-    rentals.sort((a, b) => a.id.localeCompare(b.id));
+    reservations.sort((a, b) => a.id.localeCompare(b.id));
   } catch (err) {
     console.log(err);
   }
